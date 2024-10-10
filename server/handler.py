39953ee734f4777d8b8b42b2619a4369b45f7310
kimroniny <1301862177@qq.com>
2024-10-10 18:26:11 +0800

    add base handler

---
+++ server/handler.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+from typing import List, Dict, Any, TypeAlias
+from server.response import Response
+
+ResponseContent: TypeAlias = str | bytes
+
+class Handler:
+    def __init__(self, url: str, method: str, arguments: Dict[str, Any]):
+        self.__url = url
+        self.__method = method
+        self.__arguments = arguments
+        self.__output = ""
+
+    def get_url(self) -> str:
+        return self.__url
+
+    def get_method(self) -> str:
+        return self.__method
+
+    def get_argument(self, key: str) -> Any:
+        return self.__arguments.get(key)
+    
+    def get_arguments(self, key: str) -> List[Any]:
+        return self.__arguments.get(key, [])
+
+    def get_output(self) -> ResponseContent:
+        return self.__output
+    
+    def write(self, content: ResponseContent):
+        assert isinstance(content, ResponseContent)
+        self.__output = content
+
+    def initialize(self, *args, **kwargs):
+        pass
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
