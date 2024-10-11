ddb423ae2fdf88c8c7e5e0d2fa4cb902d8d717c3
kimroniny <1301862177@qq.com>
2024-10-11 18:54:29 +0800

    another solution: only build connection once

--- client/client.py
+++ client/client.py
@@ -6,6 +6,7 @@ from multiprocessing.connection import Client as MClient, Connection
 class Client:
     def __init__(self, socket_path):
         self.socket_path = socket_path
+        # self.client: Connection = MClient(self.socket_path)
 
     def get(self, url: str, params: Dict[str, Any]) -> Response:
         return self.send(url, "GET", params)
@@ -13,6 +14,18 @@ class Client:
     def post(self, url: str, params: Dict[str, Any]) -> Response:
         return self.send(url, "POST", params)
 
+    def send2(self, url: str, method: str, params: Dict[str, Any]) -> Response:
+        message = {
+            "method": method,
+            "url": url,
+            "params": params,
+        }
+        self.client.send(message)
+        response = self.client.recv()
+        response = Response.from_str(response)
+        # print(f"收到服务器响应, code: {response.code}, msg: {response.msg}, err: {response.err}")
+        return response
+    
     def send(self, url: str, method: str, params: Dict[str, Any]) -> Response:
         client: Connection = MClient(self.socket_path)
         message = {
@@ -26,3 +39,7 @@ class Client:
         # print(f"收到服务器响应, code: {response.code}, msg: {response.msg}, err: {response.err}")
         client.close()
         return response
+    
+    # def __del__(self):
+    #     if self.client is not None:
+    #         self.client.close()
