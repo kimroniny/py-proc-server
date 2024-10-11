ca7cc03b7bdf1b57c187466eebf3dfb833288b45
kimroniny <1301862177@qq.com>
2024-10-11 23:51:51 +0800

    adapt to the new server

--- example/test_client.py
+++ example/test_client.py
@@ -1,6 +1,11 @@
 import threading
 from client.client import Client
 
+def get(client: Client, url: str, params: dict):
+    response = client.get(url, params)
+    print(f"收到响应: {response.code}, {response.msg}, {response.err}")
+    return response
+
 class ClientSimulator:
     def __init__(self, socket_path, num_clients):
         self.socket_path = socket_path
@@ -14,7 +19,7 @@ class ClientSimulator:
             params = {'msg': message}
             url = "/"
             # print(f"发送消息长度: {len(message)}")
-            t = threading.Thread(target=client.get, args=(url, params))
+            t = threading.Thread(target=get, args=(client, url, params))
             threads.append(t)
             t.start()
 
@@ -23,6 +28,6 @@ class ClientSimulator:
 
 if __name__ == "__main__":
     socket_path = "/tmp/server_socket"
-    num_clients = 10  # 模拟100个客户端
+    num_clients = 100  # 模拟100个客户端
     simulator = ClientSimulator(socket_path, num_clients)
     simulator.run()
