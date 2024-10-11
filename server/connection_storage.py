91dccda8bde6302c335f75bd9b209988aef4ffb3
kimroniny <1301862177@qq.com>
2024-10-11 23:52:15 +0800

    supprt connection cache storage

---
+++ server/connection_storage.py
@@ -0,0 +1,80 @@
+import queue
+import threading
+import time
+import traceback
+from loguru import logger
+from multiprocessing.connection import Connection
+from typing import Dict, Set, Any, Tuple
+
+class ConnectionStorage:
+    def __init__(self, max_size: int=100):
+        self.storage: Set[Connection] = set()
+        self.max_size: int = max_size
+        self.exist_in_queue: Dict[Connection, bool] = {}
+        self.available_conns: queue.Queue = queue.Queue()
+        self.stop_flag: threading.Event = None
+        self.storage_lock = threading.RLock()
+        self.__closed = False
+
+    def add(self, conn: Connection) -> int:
+        if self.__closed:
+            return 1
+        if len(self.storage) >= self.max_size:
+            logger.warning(f"connection storage is full(size={len(self.storage)})")
+            return 2
+        with self.storage_lock:
+            self.storage.add(conn)
+            self.exist_in_queue[conn] = False
+        return 0
+
+    def remove(self, conn: Connection):
+        with self.storage_lock:
+            try:
+                if conn not in self.storage:
+                    return
+                self.storage.remove(conn)
+                del self.exist_in_queue[conn]
+                conn.close()
+            except Exception as e:
+                logger.error(f"Error removing and closing connection in ConnectionStorage: {e}")
+        
+    def init_stop_flag(self, stop_flag: threading.Event):
+        self.stop_flag = stop_flag
+
+    def get_msg_from_available_conn_queue(self) -> Tuple[Connection, Any]:
+        while True:
+            conn: Connection = self.available_conns.get()
+            with self.storage_lock:
+                if conn in self.storage and not conn.closed:
+                    print(f"connection is polled: {conn.poll()}, closed: {conn.closed}, fileno: {conn.fileno()}")
+                    msg = conn.recv()
+                    # 从conn中接收完所有消息后, 才可以设置为 false, 此时 poll 线程才可以重新将 conn 加入到 available_conns 队列中
+                    self.exist_in_queue[conn] = False 
+                    return conn, msg
+
+    def __close_conns(self):
+        with self.storage_lock:
+            try:
+                for conn in self.storage:
+                    conn.close()
+                self.storage.clear()
+            except Exception as e:
+                logger.error(f"Error closing connection in ConnectionStorage: {e}")
+            finally:
+                self.__closed = True
+
+    def poll(self):
+        assert self.stop_flag is not None
+        try:
+            while not self.stop_flag.is_set():
+                with self.storage_lock:
+                    for conn in self.storage:
+                        if not self.exist_in_queue.get(conn, False) and conn.poll(): # 先判断是否在队列中, 再判断是否可读
+                            self.available_conns.put(conn)
+                            self.exist_in_queue[conn] = True
+                time.sleep(0)
+        except Exception as e:
+            logger.error(f"Error in poll: {traceback.format_exc()}")
+        finally:
+            self.__close_conns()
+            self.stop_flag.set()
--- server/server.py
+++ server/server.py
@@ -2,9 +2,11 @@ from __future__ import annotations
 import os
 import json
 import sys
+import traceback
+import time
 import threading
 import multiprocessing
-import atexit
+import traceback
 import os
 from loguru import logger
 from typing import List, Tuple, Dict, Any, TypeAlias, Optional
@@ -13,31 +15,42 @@ import signal
 from concurrent.futures import ThreadPoolExecutor
 from server.response import Response
 from server.handler import Handler
+from server.connection_storage import ConnectionStorage
 
 
 Route: TypeAlias = Tuple[str, Handler, Dict[str, Any]]
 
 class Server:
-    def __init__(self, routes: List[Route], max_workers: Optional[int]=None):
+    def __init__(self, routes: List[Route], max_workers: Optional[int]=None, max_conns: Optional[int]=None):
         self.routes_map: dict[str, Tuple[Handler, Dict[str, Any]]] = {url: (handler, args) for url, handler, args in routes}
         self.max_workers = max_workers or multiprocessing.cpu_count() * 2  # 设置线程池大小
+        self.connection_storage = ConnectionStorage(max_size=max_conns or self.max_workers)
         self.executor = ThreadPoolExecutor(max_workers=self.max_workers)
 
-    def setup(self, socket_path: str) -> bool:
+    def __setup(self, socket_path: str, stop_event: threading.Event) -> bool:
         if os.path.exists(socket_path):
             logger.error(f"socket path {socket_path} already exists, remove it")
             return False
         self.server = Listener(family='AF_UNIX', address=socket_path)
+        self.connection_storage.init_stop_flag(stop_event)
         logger.debug(f"服务器正在监听 {socket_path}")
         return True
 
-    def listen(self, socket_path: str, stop_event: threading.Event):
+    def __listen_accept(self, stop_event: threading.Event):
         try:
-            if not self.setup(socket_path): return
             while not stop_event.is_set():
                 try:
                     c = self.server.accept()
                 except OSError:
+                    continue
+                ret = self.connection_storage.add(c)
+                if ret == 1:
+                    logger.warning("connection storage is closed, refuse and close the connection")
+                    c.close()
+                    continue
+                elif ret == 2:
+                    logger.warning("connection storage is full, refuse and close the connection")
+                    c.close()
                     continue
                 self.executor.submit(self.__handle, c)
         except (KeyboardInterrupt, SystemExit):
@@ -46,20 +59,38 @@ class Server:
         finally:
             stop_event.set()
             logger.info("服务器已关闭")
-
-    def __handle(self, conn: Connection):
+    
+    def __poll_data(self, stop_event: threading.Event):
+        thread_poll = threading.Thread(target=self.connection_storage.poll)
+        thread_poll.start()
         try:
-            data = conn.recv()
+            while not stop_event.is_set():
+                item: Tuple[Connection, Any] = self.connection_storage.get_msg_from_available_conn_queue()
+                self.executor.submit(self.__handle, item)
+        except Exception as e:
+            logger.error(f"Error in poll data: {traceback.format_exc()}")
+            stop_event.set()
+
+    def __handle(self, item: Tuple[Connection, Any]):
+        conn, data = item
+        try:
             logger.debug(f"recv data: {data}")
+
+            if data == 'close':
+                conn.send("ok")
+                self.connection_storage.remove(conn)
+                return
+
             response = self.__process_data(data)
             resp_str = response.to_str()
             conn.send(resp_str)
             logger.debug(f"send response: {resp_str}")
         except Exception as e:
-            logger.error(f"Error in handling connection: {e}")
+            logger.error(f"Error in handling connection: {traceback.format_exc()}")
         finally:
-            if conn and not conn.closed:
-                conn.close()  # 确保连接在处理完后关闭
+            # if conn and not conn.closed:
+            #     conn.close()  # 确保连接在处理完后关闭
+            pass
     
     def __process_data(self, data) -> Response:
         resp = Response(404)
@@ -89,8 +120,33 @@ class Server:
                 del handler
             logger.debug(f"process data return: {resp}")
             return resp
+        
+    def start(self, socket_path: str, stop_event: threading.Event):
+        self.stop_event = stop_event
+        
+        if not self.__setup(socket_path, stop_event): return
+
+        thread_poll_data = threading.Thread(target=self.__poll_data, args=(stop_event, ))
+        thread_poll_data.start()
+
+        thread_listen_accept = threading.Thread(target=self.__listen_accept, args=(stop_event, ))
+        thread_listen_accept.start()
+
+        try:
+            while not self.stop_event.is_set():
+                time.sleep(1)
+        except KeyboardInterrupt:
+            self.stop()
+
+        thread_poll_data.join()
+        thread_listen_accept.join()
+    
+    def stop(self):
+        if self.stop_event:
+            self.stop_event.set()
 
 if __name__ == "__main__":
     socket_path = "/tmp/server_socket"
-    server = Server()
-    server.listen(socket_path)
+    server = Server(20)
+    stop_event = threading.Event()
+    server.start(socket_path, stop_event)
