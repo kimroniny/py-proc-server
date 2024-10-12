2673449d2a975f0e7241180607fb7980aa6674c8
kimroniny <1301862177@qq.com>
2024-10-12 23:59:58 +0800

    update client, add 1. reuse connection 2. when closing, negotiate with the server

--- client/client.py
+++ client/client.py
@@ -2,43 +2,58 @@
 from server.response import Response
 from typing import Dict, Any
 from loguru import logger
+import time
+import multiprocessing
+import threading
 from multiprocessing.connection import Client as MClient, Connection
 
 class Client:
-    def __init__(self, socket_path):
+    def __init__(self, socket_path, reuse_client=False):
         self.socket_path = socket_path
-        # self.client: Connection = MClient(self.socket_path)
+        self.reuse_client = reuse_client
+        self.__client = None
+        if self.reuse_client:
+            self.__client: Connection = MClient(self.socket_path)
 
     def get(self, url: str, params: Dict[str, Any]) -> Response:
         return self.send(url, "GET", params)
 
     def post(self, url: str, params: Dict[str, Any]) -> Response:
         return self.send(url, "POST", params)
+    
+    def send(self, url:str, method:str, params:Dict[str, Any]) -> Response:
+        if not self.reuse_client:
+            return self.__send_no_reuse(url, method, params)
+        else:
+            return self.__send_reuse(url, method, params)
 
-    def send2(self, url: str, method: str, params: Dict[str, Any]) -> Response:
+    def __send_reuse(self, url: str, method: str, params: Dict[str, Any]) -> Response:
         message = {
             "method": method,
             "url": url,
             "params": params,
         }
-        self.client.send(message)
-        response = self.client.recv()
+        self.__client.send(message)
+        response = self.__client.recv()
         response = Response.from_str(response)
         # print(f"收到服务器响应, code: {response.code}, msg: {response.msg}, err: {response.err}")
         return response
     
     def __close(self, client: Connection):
         try:
+            fileno = client.fileno()
+            logger.debug(f"client(fileno: {fileno}) send close message to server")
             client.send("close")
             ack = client.recv()
-            assert ack == "ok"
-            logger.info(f"client closed, client: {client.fileno()}")
+            assert str(ack).startswith("ok#")
             client.close()
+            logger.debug(f"client(fileno: {fileno}) closed, recv from server: {ack}")
         except Exception as e:
             logger.error(f"Error closing client: {e}")
 
-    def send(self, url: str, method: str, params: Dict[str, Any]) -> Response:
+    def __send_no_reuse(self, url: str, method: str, params: Dict[str, Any]) -> Response:
         client: Connection = MClient(self.socket_path)
+        logger.debug(f"process id: {multiprocessing.current_process().name}, thread id: {threading.current_thread().name}, object id: {id(self)}, create client fileno: {client.fileno()}")
         message = {
             "method": method,
             "url": url,
@@ -47,10 +62,10 @@ class Client:
         client.send(message)
         response = client.recv()
         response = Response.from_str(response)
-        # print(f"收到服务器响应, code: {response.code}, msg: {response.msg}, err: {response.err}")
         self.__close(client)
+        logger.debug(f"send request finish!")
         return response
     
-    # def __del__(self):
-    #     if self.client is not None:
-    #         self.client.close()
+    def __del__(self):
+        if self.__client:
+            self.__close(self.__client)
